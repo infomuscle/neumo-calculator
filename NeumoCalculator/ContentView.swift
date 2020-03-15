0c99067fe298670415df009f165a6ebea429b1d9
infomuscle <infomuscle10@gmail.com>
2020-03-16 00:40:29 +0900

    Completed Basic Functions

--- NeumoCalculator/ContentView.swift
+++ NeumoCalculator/ContentView.swift
@@ -73,6 +73,15 @@ struct ContentView: View {
         [["00", "00"], ["0", "0"], [".", "PNT"], ["=", "EQL"]]
     ]
 
+    @State var numbers: [String] = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "00"]
+    @State var operators: [String] = ["ADD","SUB","MUL","DIV","MOD","EQL","PNT","PM","AC",]
+
+    @State var result: String = ""
+    @State var input1: String = "0"
+    @State var input2: String = ""
+    @State var op: String = ""
+    @State var display: String = "0"
+
     var body: some View {
         ZStack {
             LinearGradient(Color.background, Color.background)
@@ -81,7 +90,7 @@ struct ContentView: View {
 
                 Spacer()
 
-                Text("0")
+                Text(display)
                         .foregroundColor(Color.foreground)
                         .padding(10)
                         .font(.system(size: 64))
@@ -108,7 +117,8 @@ struct ContentView: View {
                         HStack(spacing: 20) {
                             ForEach(line, id: \.self) { key in
                                 Button(action: {
-                                    print("Button \(key[1]) tapped")
+//                                    print("Button \(key[1]) tapped")
+                                    self.touchCommon(val: String(key[1]))
                                 }) {
                                     Text(key[0])
                                 }.buttonStyle(NeumoButtonStyle())
@@ -119,6 +129,117 @@ struct ContentView: View {
             .padding(.bottom, 50)
         }
                 .edgesIgnoringSafeArea(.all)
+    }
+
+    // Button Controller
+    func touchCommon(val: String) {
+        if numbers.contains(val) {
+            touchNumber(val: val)
+        } else if operators.contains(val) && val != "PM" {
+            touchOperation(val: val)
+        } else {
+            touchPlusMinus()
+        }
+    }
+
+    // Action When Touch 0~9 Buttons
+    func touchNumber(val: String) {
+        if (result != "" && input1 != "" && op == "") {
+            initCalculator()
+        }
+        if (op == "") {
+            if input1 == "0" {
+                input1 = ""
+            }
+            input1  += val
+            updateDisplay(val: input1)
+        } else {
+            input2 += val
+            updateDisplay(val: input2)
+        }
+
+        log()
+    }
+
+    // Action When Touch Operator Buttons
+    func touchOperation(val: String) {
+        if (val == "AC") {
+            initCalculator()
+            log()
+            return
+        }
+
+        if (val != "EQL") {
+            op = val
+        }
+
+        if (input2 != "") {
+            switch (op) {
+            case "ADD":
+                result = String(Int(input1)! + Int(input2)!)
+                break
+            case "SUB":
+                result = String(Int(input1)! - Int(input2)!)
+                break
+            case "MUL":
+                result = String(Int(input1)! * Int(input2)!)
+                break
+            case "DIV":
+                result = String(Int(input1)! / Int(input2)!)
+                break
+            case "MOD":
+                result = String(Int(input1)! % Int(input2)!)
+                break
+            default:
+                break
+            }
+        }
+
+        if (input1 != "" && input2 != "") {
+            updateDisplay(val: result)
+        }
+
+        if (input2 != "" && val == "eql") {
+            op = ""
+            input1 = result
+            input2 = ""
+        } else if (input2 != "") {
+            input1 = result
+            input2 = ""
+        }
+
+        log()
+    }
+
+    // Action When Touch +/- Button
+    func touchPlusMinus() {
+        result = String(Int(result)! * (-1))
+        updateDisplay(val: result)
+        log()
+    }
+
+    // Set Display Value
+    func updateDisplay(val: String) {
+        display = val
+    }
+
+    // Initialize Variables
+    func initCalculator() {
+        result = ""
+        input1 = "0"
+        input2 = ""
+        op = ""
+        updateDisplay(val: "0")
+    }
+
+    // Logger for Debugging
+    func log() {
+        print("result : " + result)
+        print("input1 : " + input1)
+        print("input2 : " + input2)
+        print("op : " + op)
+        print("display : " + display)
+        print("------------------")
     }
 }
 
